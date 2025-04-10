67ab08a2352708786e3488491fa90189eabc4bdc
Nguyễn Tân Duy <duy940210714@gmail.com>
2025-04-10 18:10:30 +0700

    Add migration for sql

---
+++ DocumentStoreManagement.Infrastructure/Migrations/20250410110503_AddDocumentAndOrderTables.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentStoreManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentAndOrderTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    PublisherName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ReleaseQuantity = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    AuthorName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    PageNumber = table.Column<int>(type: "int", nullable: true),
+                    ReleaseNumber = table.Column<int>(type: "int", nullable: true),
+                    ReleaseMonth = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(15)", unicode: false, maxLength: 15, nullable: false),
+                    BorrowDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(18,4)", nullable: false),
+                    DocumentId = table.Column<string>(type: "nvarchar(128)", nullable: false),
+                    OrderId = table.Column<string>(type: "nvarchar(128)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Documents_DocumentId",
+                        column: x => x.DocumentId,
+                        principalTable: "Documents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_DocumentId",
+                table: "OrderDetails",
+                column: "DocumentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Documents");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
--- DocumentStoreManagement.Infrastructure/SqlApplicationContext.cs
+++ DocumentStoreManagement.Infrastructure/SqlApplicationContext.cs
@@ -13,5 +13,35 @@ namespace DocumentStoreManagement.Infrastructure
         /// Student Table
         /// </summary>
         public DbSet<Student> Students { get; set; }
+
+        /// <summary>
+        /// Document Table
+        /// </summary>
+        public DbSet<Document> Documents { get; set; }
+
+        /// <summary>
+        /// Book Table
+        /// </summary>
+        public DbSet<Book> Books { get; set; }
+
+        /// <summary>
+        /// Magazine Table
+        /// </summary>
+        public DbSet<Magazine> Magazines { get; set; }
+
+        /// <summary>
+        /// Newspaper Table
+        /// </summary>
+        public DbSet<Newspaper> Newspapers { get; set; }
+
+        /// <summary>
+        /// Order Table
+        /// </summary>
+        public DbSet<Order> Orders { get; set; }
+
+        /// <summary>
+        /// OrderDetail Table
+        /// </summary>
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
